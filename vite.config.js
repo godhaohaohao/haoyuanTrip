5a9cb5aa76a2c4ccf621e0157ac72d25b1032ce8
hao <971536553@qq.com>
2023-02-15 15:07:10 +0800

    第一次

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,13 @@ import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./", //等同于  assetsPublicPath :'./'
   plugins: [
     vue(),
     Components({
       resolvers: [VantResolver()],
     }),
   ],
-  base:'/haoyuanTrip',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
